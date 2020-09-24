f83ad1c661c5a3f81667c609fdd24cab139e11d7
Patrick <aschkan90@yahoo.de>
2020-09-24 14:01:25 +0200

    cancellation

--- register.php
+++ register.php
@@ -1,18 +1,27 @@
 <?php
-    include './include/dbconnect.php';
+    include("include/dbconnect.php");
 
-    SESSION_START();
+    if(empty($_POST["submit"])){
+        // Saving User
+        $username = $_GET["username"];
+        $password = $_GET["password"];
 
-    $username = $_GET['username'];
-    $password = $_GET['password'];
-    
-    if(!empty($_POST["submit"])){
-        
-        $sql = "INSERT INTO userverwaltung (Username,Passwort) VALUES 
-        ('$username','$password')       
-        ";
-        $res = mysqli_query($db, $sql);
+        // Checking if User Exists
 
+        $sql_select_user = "SELECT username FROM userverwaltung WHERE username = '$username'";
+        $sql_query_user = mysqli_query($db, $sql_select_user);
+
+        if(mysqli_num_rows($sql_query_user) > 0) {
+           echo "user already exist";
+           header('Location: index.php');
+        } else {
+            $sql = "INSERT INTO `heintzetest`.`userverwaltung` (`Username`, `Passwort`) VALUES ('$username', '$password')";
+
+            $insert = mysqli_query($db, $sql);
+            header('Location: welcome.php');
+        }
     }
+
+  
     
 ?>
