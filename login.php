e074965e358de914eb1f14897f377622b7a829da
Patrick <aschkan90@yahoo.de>
2020-09-24 13:31:26 +0200

    include path correction

--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <?php
-    include("include/dbconnet.php");
+    include './include/dbconnect.php';
+
+    SESSION_START();
+
     $username = $_GET['username'];
     $password = $_GET['password'];
     
@@ -8,8 +11,13 @@
         $sql = "SELECT * FROM userverwaltung WHERE
                 Username ='$username' AND
                 Passwort ='$password'
-        "
-        $res = mysql_query($sql, $db)
+        ";
+        $res = mysql_query($sql, $db);
+        $anzahl = @mysql_num_rows($res);
+
+        if($anzahl > 0){
+            //Welcome screen loading 
+        }
 
     }
     
