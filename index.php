4a99fb1b6437eee6efb0200596ebe5c6a578264d
Christian <c.volkenandt@web.de>
2020-09-24 12:24:12 +0200

    Made index and Stylesheet

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<?php
-phpinfo();
-$x = "lol";
-if ($x = "lol") {
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
 
-    echo x;
-}
-?>
+    <body>
+        
+    <div id="loginForm">
+        <form action="login.php">
+            <label for="username">Username:</label><br>
+            <input type="text" id="username" name="username" value=""><br>
+            <label for="password">Password:</label><br>
+            <input type="text" id="password" name="password" value=""><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+
+    </body>
+</html>
